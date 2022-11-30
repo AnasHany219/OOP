4e723137c09173b53e6e8ceb10811e444b70595e
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2022-11-30 22:08:45 +0200

    Create Struct_AddDistance.cpp

---
+++ Struct_AddDistance.cpp
@@ -0,0 +1,38 @@
+// Struct in Function
+#include <iostream>
+using namespace std;
+struct Distance
+{
+// Attributes
+        int feet;
+        float inches;
+};
+//struct function
+Distance add_distance(Distance d1, Distance d2)
+{
+// Declare struct
+    Distance result;
+    result.feet = d1.feet + d2.feet;
+    result.inches = d1.inches + d2.inches;
+// return 2 value
+    return result;
+}
+int main()
+{
+    Distance x, y, z;
+    cout << "Enter feet values : \n";
+    cin >> x.feet >> y.feet;
+    cout << "Enter inches values : \n";
+    cin >> x.inches >> y.inches;
+    z = add_distance(z, y);
+    cout << "z.feet = " << z.feet << " z.inches = " << z.inches << "\n";
+    /*
+    ---------Output---------
+    Enter feet values :
+    "User enter" -> 10 12
+    Enter inches values :
+    "User Inter" -> 5.41 9.12
+    z.feet = 50 z.inches = 9.12
+    */
+    return 0;
+}
