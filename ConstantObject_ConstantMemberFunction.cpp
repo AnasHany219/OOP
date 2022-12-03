9538a461605a2f4c74585e1a80a2dd4d39f53ad9
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2022-12-03 19:36:35 +0200

    Update ConstantObject_ConstantMemberFunction.cpp

--- ConstantObject_ConstantMemberFunction.cpp
+++ ConstantObject_ConstantMemberFunction.cpp
@@ -21,10 +21,10 @@ int main()
 // Constant Object
 // When we declare a constant object we cannot access functions that are not constant.
     const time noon1(12, 00, 00);
-    noon1.print();
+    noon1.print();      // The Time = 12 : 0 : 0
 // When we declare a Non-constant object we can access functions that are constant or Non-constant.
     time noon2(06, 30, 15);
-    noon2.print();
-    return 0;
+    noon2.print();      // The Time = 6 : 30 : 15
+    return 0;   
 }
 //  End of the main
