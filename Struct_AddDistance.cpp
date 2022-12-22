539bdec5b37698877905944fa864fc380759418c
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2022-12-22 14:24:21 +0200

    Update Struct_AddDistance.cpp

--- Struct_AddDistance.cpp
+++ Struct_AddDistance.cpp
@@ -24,15 +24,15 @@ int main()
     cin >> x.feet >> y.feet;
     cout << "Enter inches values : \n";
     cin >> x.inches >> y.inches;
-    z = add_distance(z, y);
+    z = add_distance(x, y);
     cout << "z.feet = " << z.feet << " z.inches = " << z.inches << "\n";
     /*
     ---------Output---------
     Enter feet values :
     "User enter" -> 10 12
     Enter inches values :
-    "User Inter" -> 5.41 9.12
-    z.feet = 50 z.inches = 9.12
+    "User Inter" -> 5.14 9.12
+    z.feet = 22 z.inches = 14.26
     */
     return 0;
 }
