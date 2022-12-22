31b7221da02e8ca94c866aa69d0b82a8cbd720b7
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2022-12-22 14:44:36 +0200

    Update and rename Constant&Non_pointer.cpp to Constant&Non-constant_pointer.cpp

--- Constant&Non-constant_pointer.cpp
+++ Constant&Non-constant_pointer.cpp
@@ -19,17 +19,17 @@ int main()
     cout << *p << "\n";             // content of the first element -> 10
     cout << *(p+2) << "\n";         // content of the third element -> 40
     cout << *p+2 << "\n";           // content of the first element + 2 -> 13
-    p += 2;
+    p += 2;                         // with third element like -> p = &arr[2]
     cout << *p << "\n";             // content of the third element -> 40
 
     p = arr;                        // with first element like -> p = &arr[0]
     for(int i = 0; i< 5; i++)
         cout << *(p+i) << " ";      //10 50 40 77 33
     cout << "\n";
+
 // Not constant
     for(int i = 0; i< 5; i++)
         cout << *(p++) << " ";      //10 50 40 77 33
-    cout << "\n";
 
 
     return 0;
