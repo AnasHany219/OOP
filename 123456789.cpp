e860e847c6840667987954134841d7c140e75e15
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2023-01-05 01:01:31 +0200

    Update 123456789.cpp

--- 123456789.cpp
+++ 123456789.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 using namespace std;
-class room{
-private:
-    int width, length;
-public:
-    void Set_Width(){
-        cin >> width;
-    }
-    void Set_Length(){
-        cin >> length;
-    }
-    void area(){
-        cout << "Area: " << width * length << "\n";
-    }
-};
+
 int main(){
-    room a;
-    cout << "Enter Width (feet): ";
-    a.Set_Width();      // 10
-    cout << "Enter Length (feet): ";
-    a.Set_Length();     // 15
+    int n, rn = 0;
+    cout << "Enter an integer: ";
+    cin >> n;   // 12345
 
-    a.area();           // 150
+    while(n > 0){
+        rn *= 10;       // 0    | 50    | 540   | 5430  | 54320
+        rn += n % 10;   // 5    | 54    | 543   | 5432  | 54321
+        n /= 10;        // 1234 | 123   | 12    | 1     | 0
+    }
 
+    cout << "The integer after reverse: " << rn;
     return 0;
 }
